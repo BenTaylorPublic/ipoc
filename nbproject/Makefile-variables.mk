85e5aae3355365f3759a208fbbe510182e6d1e72
Ben Taylor <ben@7-ym.com.au>
2017-07-01 03:19:58 +1000

    mac project files

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=ipoc-core
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/ipoc-core
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=ipoc-core.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/ipoc-core.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
+CND_ARTIFACT_NAME_Release=ipoc-core
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/ipoc-core
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Release=ipoc-core.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/ipoc-core.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
