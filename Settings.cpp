8305b46db332a0a5aeb59629b78e8ab050cb91e1
Ben Taylor <ben@7-ym.com.au>
2017-07-10 09:06:41 +1000

    Official version change to V0.5.0

--- Settings.cpp
+++ Settings.cpp
@@ -14,7 +14,7 @@
 #endif 
 
 //All the defaults
-std::string Settings::ipocVersion = "0.4.0";
+std::string Settings::ipocVersion = "0.5.0";
 
 std::string Settings::imageFilePath = "default file path";
 std::string Settings::fontFilePath = "default file path";
