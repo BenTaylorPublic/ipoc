f8d35050f6725caecdb38b6ce0c084ad4b32116d
Ben Taylor <benjaminjacktaylor@hotmail.com>
2017-07-24 17:48:37 +1000

    Mac: Initial commit for new minor development branch

--- Settings.cpp
+++ Settings.cpp
@@ -14,7 +14,7 @@
 #endif 
 
 //All the defaults
-std::string Settings::ipocVersion = "0.7.0";
+std::string Settings::ipocVersion = "0.7.0"; //Obligatory initial branch commit
 
 std::string Settings::imageFilePath = "default file path";
 std::string Settings::fontFilePath = "default file path";
