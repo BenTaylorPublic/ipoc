0571a412539941b348583239c0c8f0938c8213f8
Ben Taylor <benjaminjacktaylor@hotmail.com>
2018-08-19 20:36:50 +1000

    Beautified

--- src/Conversions.cpp
+++ src/Conversions.cpp
@@ -1,24 +1,7 @@
 #include "Conversions.h"
 #include "Debug.h"
 
-std::string Conversions::keyEnumStrings[AMOUNT_OF_PHYSICAL_BUTTONS] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-																	   "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-																	   "U", "V", "W", "X", "Y", "Z", "Num0", "Num1",
-																	   "Num2", "Num3", "Num4", "Num5", "Num6", "Num7",
-																	   "Num8", "Num9", "Numpad0", "Numpad1", "Numpad2",
-																	   "Numpad3", "Numpad4", "Numpad5", "Numpad6",
-																	   "Numpad7", "Numpad8", "Numpad9", "ArrowLeft",
-																	   "ArrowRight", "ArrowUp", "ArrowDown", "F1", "F2",
-																	   "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10",
-																	   "F11", "F12", "Add", "Subtract", "Divide",
-																	   "Multiply", "Escape", "BackSlash", "BackSpace",
-																	   "Comma", "Dash", "Delete", "Equal", "Home",
-																	   "Insert", "LAlt", "LBracket", "LControl",
-																	   "LShift", "LSystem", "Menu", "PageUp",
-																	   "PageDown", "Pause", "Period", "Quote", "RAlt",
-																	   "RBracket", "RControl", "Return", "RShift",
-																	   "RSystem", "SemiColon", "Slash", "Space", "Tab",
-																	   "Tilde", "End"};
+std::string Conversions::keyEnumStrings[AMOUNT_OF_PHYSICAL_BUTTONS] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Num0", "Num1", "Num2", "Num3", "Num4", "Num5", "Num6", "Num7", "Num8", "Num9", "Numpad0", "Numpad1", "Numpad2", "Numpad3", "Numpad4", "Numpad5", "Numpad6", "Numpad7", "Numpad8", "Numpad9", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Add", "Subtract", "Divide", "Multiply", "Escape", "BackSlash", "BackSpace", "Comma", "Dash", "Delete", "Equal", "Home", "Insert", "LAlt", "LBracket", "LControl", "LShift", "LSystem", "Menu", "PageUp", "PageDown", "Pause", "Period", "Quote", "RAlt", "RBracket", "RControl", "Return", "RShift", "RSystem", "SemiColon", "Slash", "Space", "Tab", "Tilde", "End"};
 
 bool Conversions::stringContains(const std::string& inputString,
 								 const std::string& containsString)
