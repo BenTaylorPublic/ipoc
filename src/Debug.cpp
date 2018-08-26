56dbbe62963fa71cc35074ee0aa181e134313a15
Ben Taylor <benjaminjacktaylor@hotmail.com>
2018-08-26 21:09:47 +1000

    Documented and sorted debug class

--- src/Debug.cpp
+++ src/Debug.cpp
@@ -19,39 +19,26 @@ unsigned int Debug::destructionAmount[AMOUNT_OF_TRACKED_CLASSES] = {0};
 
 unsigned int Debug::copyAmount[AMOUNT_OF_TRACKED_CLASSES] = {0};
 
-void Debug::write(const int& input)
+void Debug::crash(const unsigned int& crashId,
+				  const std::string& callFrom)
 {
-	std::cout << std::to_string(input);
+	writeLine("[CRASH] Program crashed. Crash ID " + std::to_string(crashId));
+	writeLine("[CRASH] Crash called from " + callFrom);
+	writeLine("[CRASH] Crashed on loop " + std::to_string(loopNumber));
+	logLine("[CRASH] Program crashed. Crash ID " + std::to_string(crashId));
+	logLine("[CRASH] Crash called from " + callFrom);
+	logLine("[CRASH] Crashed on loop " + std::to_string(loopNumber));
+	exit(EXIT_FAILURE);
 }
 
-void Debug::writeUnsigned(const unsigned int& input)
+void Debug::incrementLoopNumber()
 {
-	std::cout << std::to_string(input);
+	loopNumber++;
 }
 
-void Debug::write(const std::string& input)
+void Debug::IPOCLoad(MasterController* inputMasterController)
 {
-	std::cout << input;
-}
-
-void Debug::writeLine(const int& input)
-{
-	std::cout << std::to_string(input) + "\n";
-}
-
-void Debug::writeLineUnsigned(const unsigned int& input)
-{
-	std::cout << std::to_string(input) + "\n";
-}
-
-void Debug::writeLine(const std::string& input)
-{
-	std::cout << input + "\n";
-}
-
-void Debug::writeLoopNumber()
-{
-	std::cout << loopNumber;
+	masterController = inputMasterController;
 }
 
 void Debug::log(const int& input)
@@ -64,9 +51,21 @@ void Debug::log(const std::string& input)
 	FileManager::writeToFile(logPath, input);
 }
 
-void Debug::logUnsigned(const unsigned int& input)
+void Debug::logClassAmountInfo()
 {
-	FileManager::writeToFile(logPath, std::to_string(input));
+	logLine("[INFO] Logging class amount info");
+
+	for (int i = 0; i < AMOUNT_OF_TRACKED_CLASSES; i++)
+	{
+		log("[INFO] ");
+		if (constructionAmount[i] + copyAmount[i] != destructionAmount[i])
+		{
+			log("MEMORY LEAK, ");
+		}
+		logLine("Class " + TrackedClasses::classNames[i] + ", constructed " + std::to_string(constructionAmount[i]) + ", copied " + std::to_string(copyAmount[i]) + ", destructed " + std::to_string(destructionAmount[i]) + ".");
+	}
+
+	logLine("[INFO] End of class amount info");
 }
 
 void Debug::logLine(const int& input)
@@ -87,86 +86,6 @@ void Debug::logLineUnsigned(const unsigned int& input)
 void Debug::logLoopNumber()
 {
 	FileManager::writeToFile(logPath, std::to_string(loopNumber));
-}
-
-void Debug::newLog()
-{
-	FileManager::clearFile(logPath);
-}
-
-void Debug::logTimeStamp()
-{
-	time_t now;
-	time(&now);
-	char buf[sizeof "2011-10-08 07:07:09"];
-	strftime(buf, sizeof buf, "%F %T", localtime(&now));
-	FileManager::writeToFile(logPath, buf);
-}
-
-void Debug::incrementLoopNumber()
-{
-	loopNumber++;
-}
-
-void Debug::IPOCLoad(MasterController* input)
-{
-	masterController = input;
-}
-
-void Debug::logStatusStrings()
-{
-	FileManager::writeToFile(logPath, masterController->getStatusString());
-}
-
-void Debug::setFilePath(std::string inputPath)
-{
-	logPath = inputPath + "/generated/DebugLog.log";
-}
-
-void Debug::notifyOfConstruction(const int& classId)
-{
-	constructionAmount[classId]++;
-}
-
-void Debug::notifyOfDestruction(const int& classId)
-{
-	destructionAmount[classId]++;
-}
-
-void Debug::notifyOfCopy(const int& classId)
-{
-	copyAmount[classId]++;
-}
-
-void Debug::crash(const unsigned int& crashId,
-				  const std::string& callFrom)
-{
-	writeLine("[CRASH] Program crashed. Crash ID " + std::to_string(crashId));
-	writeLine("[CRASH] Crash called from " + callFrom);
-	writeLine("[CRASH] Crashed on loop " + std::to_string(loopNumber));
-	logLine("[CRASH] Program crashed. Crash ID " + std::to_string(crashId));
-	logLine("[CRASH] Crash called from " + callFrom);
-	logLine("[CRASH] Crashed on loop " + std::to_string(loopNumber));
-	exit(EXIT_FAILURE);
-}
-
-void Debug::logClassAmountInfo()
-{
-	logLine("[INFO] Logging class amount info");
-
-	for (int i = 0; i < AMOUNT_OF_TRACKED_CLASSES; i++)
-	{
-		log("[INFO] ");
-		if (constructionAmount[i] + copyAmount[i] != destructionAmount[i])
-		{
-			log("MEMORY LEAK, ");
-		}
-		logLine("Class " + TrackedClasses::classNames[i] + ", constructed " + std::to_string(constructionAmount[i]) +
-				", copied " + std::to_string(copyAmount[i]) + ", destructed " + std::to_string(destructionAmount[i]) +
-				".");
-	}
-
-	logLine("[INFO] End of class amount info");
 }
 
 void Debug::logMemoryLeakInfo()
@@ -198,4 +117,84 @@ void Debug::logMemoryLeakInfo()
 		logClassAmountInfo();
 		logLine("[WARNING] End of memory leak information.");
 	}
+}
+
+void Debug::logStatusStrings()
+{
+	FileManager::writeToFile(logPath, masterController->getStatusString());
+}
+
+void Debug::logTimeStamp()
+{
+	time_t now;
+	time(&now);
+	char buf[sizeof "2011-10-08 07:07:09"];
+	strftime(buf, sizeof buf, "%F %T", localtime(&now));
+	FileManager::writeToFile(logPath, buf);
+}
+
+void Debug::logUnsigned(const unsigned int& input)
+{
+	FileManager::writeToFile(logPath, std::to_string(input));
+}
+
+void Debug::newLog()
+{
+	FileManager::clearFile(logPath);
+}
+
+void Debug::notifyOfConstruction(const int& classId)
+{
+	constructionAmount[classId]++;
+}
+
+void Debug::notifyOfCopy(const int& classId)
+{
+	copyAmount[classId]++;
+}
+
+void Debug::notifyOfDestruction(const int& classId)
+{
+	destructionAmount[classId]++;
+}
+
+//TODO: make inputPath const reference
+void Debug::setFilePath(std::string inputPath)
+{
+	logPath = inputPath + "/generated/DebugLog.log";
+}
+
+void Debug::write(const std::string& input)
+{
+	std::cout << input;
+}
+
+void Debug::write(const int& input)
+{
+	std::cout << std::to_string(input);
+}
+
+void Debug::writeLine(const int& input)
+{
+	std::cout << std::to_string(input) + "\n";
+}
+
+void Debug::writeLine(const std::string& input)
+{
+	std::cout << input + "\n";
+}
+
+void Debug::writeLineUnsigned(const unsigned int& input)
+{
+	std::cout << std::to_string(input) + "\n";
+}
+
+void Debug::writeLoopNumber()
+{
+	std::cout << loopNumber;
+}
+
+void Debug::writeUnsigned(const unsigned int& input)
+{
+	std::cout << std::to_string(input);
 }
--- src/Debug.h
+++ src/Debug.h
@@ -4,65 +4,186 @@
 #include "MasterController.h"
 #include "TrackedClasses.h"
 
-/*
-Debug is a Static Helper
-
-Debug is used for:
-1. Writing to console
-2. Logging to log file
-3. Dumping status strings to log on program crash
-4. Memory leak monitoring
+/**
+ * Debug is a Static Helper \n
+ * Debug is used for \n
+ * 1. Writing to console \n
+ * 2. Logging to log file \n
+ * 3. Dumping status strings to log on program crash \n
+ * 4. Memory leak monitoring
  */
 class Debug
 {
 public:
-	static void notifyOfConstruction(const int& classId); //for memory leak monitoring
-	static void notifyOfDestruction(const int& classId); //for memory leak monitoring
-	static void notifyOfCopy(const int& classId); //for memory leak monitoring
-	static void write(const int& input);
-	static void write(const std::string& input);
-	static void writeUnsigned(const unsigned int& input);
-	static void writeLine(const int& input);
-	static void writeLine(const std::string& input);
-	static void writeLineUnsigned(const unsigned int& input);
-	static void writeLoopNumber();
-	static void newLog(); //Clears the log file
-	static void log(const int& input);
-	static void log(const std::string& input);
-	static void logUnsigned(const unsigned int& input);
-	static void logLine(const int& input);
-	static void logLine(const std::string& input);
-	static void logLineUnsigned(const unsigned int& input);
-	static void logTimeStamp();
-	static void logLoopNumber();
-	static void logMemoryLeakInfo();
-	static void logClassAmountInfo();
+	/**
+	 * Crashes the program to desktop \n
+	 * Should only be used in a scenario when the program will throw an exception if it continues
+	 * @param crashId The crash id. Add them to CrashIDs.txt when made
+	 * @param callFrom A string of the location of the call. Example "Settings.getBoolFromSettings()"
+	 */
 	static void crash(const unsigned int& crashId,
 					  const std::string& callFrom);
-	/*
-	Calls every object's getStatusString() method, and logs them immediately to the log file.
-	This should be used for serious debugging issues.
-	This should also be called on any program crashes.
-	It makes use of the masterController pointer defined below.
+	/**
+	 * Increments the loopNumber \n
+	 */
+	static void incrementLoopNumber();
+	/**
+	 * <b> IPOC ONLY </b> \n
+	 * Called when the engine is starting
+	 * @param inputMasterController A point to the MasterController, which will be used to logStatusStrings() when requested
+	 */
+	static void IPOCLoad(MasterController* inputMasterController);
+	/**
+	 * Logs the input to the debug log
+	 * This differs from logUnsigned() because unsigned ints must be treated differently when being output
+	 * @param input input to output to the debug log
+	 */
+	static void log(const int& input);
+	/**
+	 * Logs the input to the debug log
+	 * @param input input to output to the debug log
+	 */
+	static void log(const std::string& input);
+	/**
+	 * <b> IPOC ONLY </b> \n
+	 * Logs to the debug log, the amount of each of the TrackedClasses
+	 */
+	static void logClassAmountInfo();
+	/**
+	 * Same as log() but appends a new line character to the end
+	 * @param input
+	 */
+	static void logLine(const int& input);
+	/**
+	 * Same as log() but appends a new line character to the end
+	 * @param input
+	 */
+	static void logLine(const std::string& input);
+	/**
+	 * Same as logUnsigned() but appends a new line character to the end
+	 * @param input
+	 */
+	static void logLineUnsigned(const unsigned int& input);
+	/**
+	 * Outputs the current loop number to the debug log
+	 */
+	static void logLoopNumber();
+	/**
+	 * <b> IPOC ONLY </b> \n
+	 * Logs to the debug log, memory leak information of the TrackedClasses
+	 */
+	static void logMemoryLeakInfo();
+	/**
+	 * Calls every object's getStatusString() method, and logs them immediately to the log file. \n
+	 * This should be used for serious debugging issues. \n
+	 * This should also be called on any program crashes. \n
+	 * It makes use of the masterController pointer defined below.
 	 */
 	static void logStatusStrings();
-	static void
-	incrementLoopNumber(); //Increments the loop number. Used for logging and writing loop numbers from anwywhere in the program.
-	static void
-	IPOCLoad(MasterController* input); //Used to received a pointer tot he MasterController. This pointer will then be used to logStatusStrings(), when requested.
-	static void setFilePath(std::string inputPath); //used to receive the file path from the Settings class.
-private:
-	static unsigned int loopNumber; //Used for logging and writing loop numbers from anwywhere in the program.
+	/**
+	 * Outputs the current time stamp to the debug log
+	 */
+	static void logTimeStamp();
+	/**
+	 * Logs the input to the debug log
+	 * This differs from log(const int& input) because unsigned ints must be treated differently when being output
+	 * @param input input to output to the debug log
+	 */
+	static void logUnsigned(const unsigned int& input);
+	/**
+	 * <b> IPOC ONLY </b> \n
+	 * Creates a new log file \n
+	 * If a log file exists, it'll reset it to an empty file
+	 */
+	static void newLog();
+	/**
+	 * This function is used to keep track of the TrackedClasses, which are monitored for memory leaks \n
+	 * Specifically this function keeps track of the construction of a TrackedClass
+	 * @param classId class ID of the class being constructed. See TrackedClasses for IDs.
+	 */
+	static void notifyOfConstruction(const int& classId);
+	/**
+	 * This function is used to keep track of the TrackedClasses, which are monitored for memory leaks \n
+	 * Specifically this function keeps track of the copying of a TrackedClass
+	 * @param classId class ID of the class being copied. See TrackesClasses for IDs.
+	 */
+	static void notifyOfCopy(const int& classId);
+	/**
+	 * This function is used to keep track of the TrackedClasses, which are monitored for memory leaks \n
+	 * Specifically this function keeps track of the destruction of a TrackedClass
+	 * @param classId class ID of the class being destructed. See TrackedClasses for IDs.
+	 */
+	static void notifyOfDestruction(const int& classId);
+	/**
+	 * <b> IPOC ONLY </b> \n
+	 * Called when the engine is starting \n
+	 * Called from settings to receive the file path for the debug log
+	 * @param inputPath
+	 */
+	static void setFilePath(std::string inputPath);
+	/**
+	 * Writes to the console, used for debug only \n
+	 * This differs from writeUnsigned() because unsigned ints must be treated differently when being output
+	 * @param input int input
+	 */
+	static void write(const int& input);
+	/**
+	 * Writes to the console, used for debug only
+	 * @param input string input
+	 */
+	static void write(const std::string& input);
+	/**
+	 * Same as write() but appends a new line character to the end
+	 * @param input
+	 */
+	static void writeLine(const int& input);
+	/**
+	 * Same as write() but appends a new line character to the end
+	 * @param input
+	 */
+	static void writeLine(const std::string& input);
+	/**
+	 * Same as writeUnsigned() but appends a new line character to the end
+	 * @param input
+	 */
+	static void writeLineUnsigned(const unsigned int& input);
+	/**
+	 * Writes the current loop number to the console
+	 */
+	static void writeLoopNumber();
+	/**
+	 * Writes to the console, used for debug only \n
+	 * This differs from write(const int& input) because unsigned ints must be treated differently when being output
+	 * @param input unsigned int input
+	 */
+	static void writeUnsigned(const unsigned int& input);
 
-	/*
-	A pointer the the MasterController.
-	The reason this exists, is because the master controller contains all the controllers below it, and those controllers contain all the objects below them.
-	So having a pointer to the top of the heirachy, is essential in debugging EVERY objects status.
-	This pointer is used by Debug::logStatusStrings()
+private:
+	/**
+	 * A count of the how many constructors have been called for each individual tracked class
+	 */
+	static unsigned int constructionAmount[AMOUNT_OF_TRACKED_CLASSES];
+	/**
+	 * A count of the how many copy constructors have been called for each individual tracked class
+	 */
+	static unsigned int copyAmount[AMOUNT_OF_TRACKED_CLASSES];
+	/**
+	 * A count of the how many destructors have been called for each individual tracked class
+	 */
+	static unsigned int destructionAmount[AMOUNT_OF_TRACKED_CLASSES];
+	/**
+	 * The file path of the DebugLog.txt file
+	 */
+	static std::string logPath;
+	/**
+	 * Used for logging and writing loop numbers from anywhere in the program.
+	 */
+	static unsigned int loopNumber;
+	/**
+	 * A pointer the the MasterController \n
+	 * The reason this exists, is because the master controller contains all the controllers below it, and those controllers contain all the objects below them \n
+	 * So having a pointer to the top of the hierarchy, is essential in debugging EVERY objects status \n
+	 * This pointer is used by Debug::logStatusStrings()
 	 */
 	static MasterController* masterController;
-	static unsigned int constructionAmount[AMOUNT_OF_TRACKED_CLASSES];
-	static unsigned int destructionAmount[AMOUNT_OF_TRACKED_CLASSES];
-	static unsigned int copyAmount[AMOUNT_OF_TRACKED_CLASSES];
-	static std::string logPath; //The filepath of the DebugLog.txt file.
 };
