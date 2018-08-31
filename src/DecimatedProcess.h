4fcd374b8dcbf8f5b8c10680ba582294ba2063dd
Ben Taylor <benjaminjacktaylor@hotmail.com>
2018-08-31 14:26:18 +1000

    Started documentation for Decimated Process

--- src/DecimatedProcess.h
+++ src/DecimatedProcess.h
@@ -2,16 +2,46 @@
 
 #include "Identifiable.h"
 
+/**
+ * DecimatedProcess is an interface to inherit from \n
+ * It's used to run a process method, every X loops (X is otherwise known as counterGoal)
+ */
 class DecimatedProcess : public Identifiable
 {
 public:
+	/**
+	 * Constructor for the DecimatedProcess interface
+	 */
 	DecimatedProcess();
+	/**
+	 * Destructor for the DecimatedProcess interface
+	 */
 	virtual ~DecimatedProcess();
-	void tick();
+	/**
+	 * Sets the counter goal to reach. When it reaches that goal, it'll call process()
+	 * @param x Calls the process method every X game loops
+	 */
 	void setProcessEveryXLoops(const unsigned int& x);
+	/**
+	 * <b> IPOC ONLY </b> \n
+	 * Ticks every loops
+	 */
+	void tick();
+	/**
+	 * Boolean, whether or not it should call the process before or after the ProcessController process() method
+	 */
 	bool tickBeforeLoop;
 private:
+	/**
+	 * djngkjsd
+	 */
 	virtual void process() = 0;
+	/**
+	 * kmldksgsd
+	 */
 	unsigned int counter;
+	/**
+	 * dsgds
+	 */
 	unsigned int counterGoal;
 };
